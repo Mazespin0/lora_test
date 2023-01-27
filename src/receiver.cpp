7cdf38a4979adc5aa4aa02d65b530e4a633e1c31
Mazespin0 <mazespin@bk.ru>
2023-01-27 18:27:39 +0800

    1

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -3,12 +3,14 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-#define LORA_SS_PIN 5
-#define LORA_RESET_PIN 15
+#define LORA_SS_PIN 6
+#define LORA_RESET_PIN 5
 #define LORA_DIO0_PIN -1 // DIO0 pin is optional, it is only needed for receive callback mode.
-#define LORA_SCK_PIN 18
-#define LORA_MISO_PIN 19
-#define LORA_MOSI_PIN 23
+#define LORA_SCK_PIN 13
+#define LORA_MISO_PIN 12
+#define LORA_MOSI_PIN 11
+#define signalBandwidth 250E3
+#define spreadingFactor 10
 
 void setup(){
   Serial.begin(115200);
@@ -21,6 +23,9 @@ void setup(){
   while (!LoRa.begin(868E6)){
     Serial.println("Starting LoRa failed!");
   }
+  
+  LoRa.setSpreadingFactor(spreadingFactor);
+  LoRa.setSignalBandwidth(signalBandwidth);
 
   Serial.println("LoRa initializing ok");
 }
